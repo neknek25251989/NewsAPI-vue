6e5552cee978343cf53e86f3847ca6d0402dd4f0
Lo Azer <neknek25251989@gmail.com>
2019-08-06 02:09:16 +0800

    get response

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <Menu></Menu>
+        <news-list v-bind:info="info"></news-list>
+
+
+    </div>
+</template>
+
+<script>
+    import Menu from './menu.vue';
+    import NewsList from './newsList.vue';
+    export default {
+        name: 'app',
+        data() {
+            return {
+                info: [],
+            }
+        },
+        components: {
+            Menu,
+            NewsList
+        },
+        mounted() {
+            console.clear();
+            const url = 'https://newsapi.org/v2/top-headlines';
+
+            this.axios.get(url, {
+                    params: {
+                        country: 'tw',
+                        apiKey: '357af6ad6bcb4fdf8945b9c5f1b8d3d1',
+                        category: 'business'
+                    }
+                })
+                .then(response => this.info = response.data.articles)
+
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/newsList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item, index) in info" :key="index">{{item.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['info']
+    }
+</script>
